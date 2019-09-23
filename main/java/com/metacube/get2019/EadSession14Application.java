60a91861375358079786a3585bf789ab273a669d
meta-vertika-goyal <52689030+meta-vertika-goyal@users.noreply.github.com>
2019-09-23 18:39:14 +0530

    EAD-Session-14 assignment submission

---
+++ main/java/com/metacube/get2019/EadSession14Application.java
@@ -0,0 +1,13 @@
+package com.metacube.get2019;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EadSession14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EadSession14Application.class, args);
+	}
+
+}
