09257a910b3469761f92a668fa04f61f2df02e8c
meta-vertika-goyal <52689030+meta-vertika-goyal@users.noreply.github.com>
2019-09-25 09:02:57 +0530

    Add files via upload

---
+++ UI-Session-2/parking_js.js
@@ -0,0 +1,16 @@
+function collapse(id){
+	
+	if(id==="employee_registration"){
+
+		
+		var form=document.getElementById("EmployeeRegistration");
+		if(form.style.display=="none"){
+			
+			form.style.display="block";
+		}
+			else if(form.style.display=="block"){
+				form.style.display="none";
+			}
+		}
+		
+	}
