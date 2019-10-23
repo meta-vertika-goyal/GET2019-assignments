6a45a4b506d0721758f348970e8710851e3e0589
meta-vertika-goyal <52689030+meta-vertika-goyal@users.noreply.github.com>
2019-10-23 18:53:53 +0530

    Add files via upload

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Product } from '../Product';
+import { ShoppingSiteService } from '../shopping-site.service';
 
 
 @Component({
@@ -9,27 +10,21 @@ import { Product } from '../Product';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-  private response:any;
-  constructor(private http:HttpClient,private product:Product) { 
-    this.product=new Product();
+ 
+  private products:Product[]=[];
+  constructor(private http:HttpClient,private service:ShoppingSiteService) { 
+    let obs=this.http.get<Product[]>('http://localhost:8080/UI-Session-11/rest/home')
+    .subscribe((response)=>response.forEach(product => {
+      this.products.push(product)}));
+      ShoppingSiteService.products=this.products;
   }
 
   ngOnInit() {
-    let obs=this.http.get('http://localhost:8080/UI-Session-11/rest/home');
-    obs.subscribe((response)=>this.response=response);
-    this.product.product_id=this.response.product_id;
-    this.product.name=this.response.product_name;
-    this.product.price=this.response.product_price;
-    this.product.description=this.response.description;
-    const byteString = window.atob(base64);
-   const arrayBuffer = new ArrayBuffer(byteString.length);
-   const int8Array = new Uint8Array(arrayBuffer);
-   for (let i = 0; i < byteString.length; i++) {
-     int8Array[i] = byteString.charCodeAt(i);
-   }
-   const blob = new Blob([int8Array], { type: 'image/jpeg' });    `
-    var blob1 = new Blob([new Uint16Array(this.response[0].image.binaryData)]);  
-    const imageUrl = URL.createObjectURL(blob1);  
-    this.product.image=imageUrl;  
+  }
+  findQuantityInCart(product_id){
+    this.service.findQuantityInCart(product_id);
+  }
+  addToCart(product_id){
+    this.service.addToCart(product_id);
   }
 }
